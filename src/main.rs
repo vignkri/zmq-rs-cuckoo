7024314a202452f23a2861a035a57bce7f217fed
vignkri <vignesh@hybridgreentech.com>
2023-07-01 13:42:23 +0200

    Handle a single publisher to multiple subcriber pattern

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,7 @@ use crate::telegram::Telegram;
 mod telegram;
 mod topic;
 
-
-const XPUB_PROXY: &'static str = "inproc://pub.proxy";
-const XSUB_PROXY: &'static str = "inproc://sub.proxy";
+const BACKEND: &'static str = "tcp://127.0.0.1:2400";
 
 pub struct Metrics {
     n_msgs: u128,
@@ -49,9 +47,9 @@ impl Metrics {
 fn run_publisher(topic: Topic, endpoint: &str) -> anyhow::Result<()> {
 
     let publisher = Context::new().socket(zmq::PUB).unwrap();
-    publisher.connect(endpoint).expect("unable to bind to socket");
+    publisher.bind(endpoint).expect("unable to bind to socket");
 
-    let message_interval = channel::tick(std::time::Duration::from_millis(1));
+    let message_interval = channel::tick(std::time::Duration::from_millis(10));
     let metrics_interval = channel::tick(std::time::Duration::from_secs(30));
 
     let mut metrics = Metrics::new();
@@ -97,35 +95,32 @@ pub fn run_client(client_id: i32, endpoint: &str) -> anyhow::Result<()> {
     // subscriber.set_rcvtimeo(60)?;
     let mut metrics = Metrics::new();
 
-    loop {
+    while true {
 
-        let msg = subscriber.recv_multipart(zmq::SNDMORE);
-        if msg.is_err() {
-            tracing::info!("Couldn't receive: {:?}", msg);
-            break;
+        if let Ok(content) = subscriber.recv_multipart(zmq::SNDMORE) {
+            let data = content.iter()
+                .map(|v| String::from_utf8(v.to_owned()).unwrap())
+                .collect::<Vec<String>>();
+
+            let payload = data.get(1).unwrap().to_owned();
+            let telegram = serde_json::from_str::<Telegram>(&payload)?;
+            let time_since_publish = telegram.handled_at();
+
+            match telegram.get_payload() {
+                telegram::Message::Value => {
+                    metrics.increment();
+                    metrics.add(time_since_publish);
+                }
+                telegram::Message::PrintMetrics => {
+                    // print the metrics
+                    let average_time = metrics.average_nanoseconds();
+                    let average_time_secs = metrics.average_secs();
+                    tracing::info!("{}: {} [{} ns] -> {} s", &client_id, &metrics.n_msgs, &average_time, &average_time_secs);
+                }
+            };
+        } else {
+            tracing::error!("unable to recv");
         }
-
-        let content = msg.unwrap();
-        let data = content.iter()
-            .map(|v| String::from_utf8(v.to_owned()).unwrap())
-            .collect::<Vec<String>>();
-
-        let payload = data.get(1).unwrap().to_owned();
-        let telegram = serde_json::from_str::<Telegram>(&payload)?;
-        let time_since_publish = telegram.handled_at();
-
-        match telegram.get_payload() {
-            telegram::Message::Value => {
-                metrics.increment();
-                metrics.add(time_since_publish);
-            }
-            telegram::Message::PrintMetrics => {
-                // print the metrics
-                let average_time = metrics.average_nanoseconds();
-                let average_time_secs = metrics.average_secs();
-                tracing::info!("{}: {} [{} ns] -> {} s", &client_id, &metrics.n_msgs, &average_time, &average_time_secs);
-            }
-        };
         
     }
 
@@ -140,30 +135,12 @@ fn main() {
     // install global collector configured based on RUST_LOG env var.
     tracing_subscriber::fmt::init();
 
-    /// Publishes to the receivers
-    let pub_proxy = Context::new().socket(zmq::XPUB).expect("Create proxy");
-    pub_proxy.bind(XPUB_PROXY).unwrap();
-
-    let sub_proxy = Context::new().socket(zmq::XSUB).expect("Create subscription proxy");
-    sub_proxy.bind(XSUB_PROXY).unwrap();
-    
     // Send a topic and then follow up with a message
     let rt = tokio::runtime::Builder::new_current_thread()
     .thread_name("clients")
     .enable_all()
     .build()
     .unwrap();
-
-
-    let publisher_topics = vec![(Topic::Core, "inproc://core"), (Topic::Events, "inproc://events")];
-    let mut publisher_handles = vec![];
-    for (pt, _ept) in publisher_topics {
-        let thh = std::thread::spawn(move || {
-            tracing::info!("Launching publisher with topic: {}", &pt);
-            run_publisher(pt, XSUB_PROXY).unwrap();
-        });
-        publisher_handles.push(thh);
-    }
 
     let client_thread = std::thread::spawn(move || { 
         tracing::info!("Starting client thread");
@@ -173,23 +150,28 @@ fn main() {
         for v in 0..2 {
             let task = handle.spawn_blocking( move || {
                 tracing::info!("Launching subscriber... {}", &v);
-                let r = run_client(v, XPUB_PROXY);
+                let r = run_client(v, BACKEND);
                 r
             });
             task_handles.push(task);
         };
-
-        loop {
-            // do nothing
-            tracing::trace!("Heartbeat on the client thread");
-            std::thread::sleep(std::time::Duration::from_secs(10));
-        }
     });
+
+    // Generate publisher
+    let publisher_topics: Vec<Topic> = vec![Topic::Core];
+    let mut publisher_handles = vec![];
+    for pt in publisher_topics {
+        let thh = std::thread::spawn(move || {
+            tracing::info!("Launching publisher with topic: {}", &pt);
+            run_publisher(pt, BACKEND).unwrap();
+        });
+        publisher_handles.push(thh);
+    }
 
     let instant = std::time::Instant::now();
     loop {
         
-        // 
+        // if elpased is more than 2 minutes, kill the system
         if instant.elapsed() > std::time::Duration::from_secs(120) {
             publisher_handles.iter().for_each(|v| drop(v));
             break;
